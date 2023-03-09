5e507a956446a37efbfa673761761e8584783ba4
Lenddy Morales <leme2003@gmail.com>
2023-03-09 10:34:08 -0500

    singly link list

--- 2 cSharp/1 linkedLists.js
+++ 2 cSharp/1 linkedLists.js
@@ -1,10 +1,8 @@
 //some comments
 
-
-
-
+//Node Class
+//creates a new node
 class ListNode{
-//some comments
     constructor(data,next = null){
         this.data = data;
         //some comments 
@@ -15,39 +13,32 @@ class ListNode{
 
 
 
-// const node = new ListNode(100)
-// console.log(node)
 
-
-//some comments
+//link list class
 
 class SinglyLinkedList{
-//some comments
+
     constructor(){
         this.head = null
         this.size = 0
     }
 
-//some comments
+//adds a node to the front of the list 
     insertAtFront(data){
-    // const current = this.head
-    // let node = new ListNode(data)
-    // if(this.isEmpty()){
-    //     this.head = node
-    // }else{
-    //     node.next = this.head
-    // }
-    // return this
     this.head = new ListNode(data, this.head)
     this.size ++;
     return this
 }
 
+
+//removes the first item in the list
     removeHead(){
         this.head = this.head.next
         this.size--
     }
 
+
+//returns the average in the list (does not work if the data is not a number )
     average(){
         if(this.isEmpty()){
             console.log(NaN)
@@ -66,6 +57,8 @@ class SinglyLinkedList{
         }
     }
 
+
+//returns true or false if the list is empty
     isEmpty(){
         if(this.head === null)return true
         else return false
@@ -73,7 +66,7 @@ class SinglyLinkedList{
 
 
 
-//some comments
+//adds a new node at the end(tail) list
     insertAtBack(data){
         const node = new ListNode(data)
         if(this.isEmpty()){
@@ -89,24 +82,24 @@ class SinglyLinkedList{
         return this
     }
 
-//some comments
+
+//adds a new node at the end(tail) list recursively  insertAtBack and insertAtBackRecursive don't stack
     insertAtBackRecursive(data,runner= this.head){
         console.log("recursion")
         if(this.isEmpty()){
             this.head = new ListNode(data)
             return this
-        }
-        if(runner.next != null){
+        }if(runner.next != null){
             this.insertAtBackRecursive(data, runner = runner.next)
-            // this.printListData()
         }else{
             this.head = new ListNode(data)
             this.size ++
             return this
         }
     }
-    
-    //some comments
+
+
+    //haven't us it yet
     insertAtBackMany(vals){
         for(const item of vals){
             this.insertAtBack(item);
@@ -116,8 +109,7 @@ class SinglyLinkedList{
 
 
 
-
-
+    //removes the last item in the list 
     removeBack() {
         if (this.isEmpty()) {
             return this;
@@ -137,12 +129,9 @@ class SinglyLinkedList{
             return this;
         }
     }
-    
 
 
-
-
-
+    //checks to see if the list contains a value
     contains(val){
         if(this.isEmpty()){
             console.log("list is empty add a new node")
@@ -161,6 +150,8 @@ class SinglyLinkedList{
         }
     }
 
+
+//checks to see if the list contains a value recursively
     containsRecursive( val, current = this.head){
         if(this.isEmpty() == true){
             console.log("the list is empty")
@@ -175,19 +166,18 @@ class SinglyLinkedList{
             return this.containsRecursive(val,current.next)
     }
 
+    //if implemented will get any value if it is on the list
     getAny(val){
         //if not empty loop over the notes
         //the note.data that has the same value as as the value inputted 
         ///sould be return 
         //if the list does not contains more thatn one node return that note 
-
         //if the value inputed is a negative int return the first node 
-
         //if it is bigger than list of node return the las node 
-
     }
 
 
+    //gets the second to last itmen in the list  
     secondToLast(){
         if(this.isEmpty()){
             console.log("list is empty")
@@ -204,6 +194,7 @@ class SinglyLinkedList{
         return previous.data
     }
 
+    //removes any values if it is on the list
     removeVal(val){
         if(this.isEmpty()){
             console.log("list is empty")
@@ -217,6 +208,10 @@ class SinglyLinkedList{
         let previous;
         while(current.next !== null){
             previous = current
+            if(previous == null ){
+                console.log(val,"not in the list")
+                return false
+            }
             current = current.next
             if(current.data == val){
                 previous.next = current.next 
@@ -228,8 +223,9 @@ class SinglyLinkedList{
     }
     }
 
-    preEnd(newVal,targetVal){
 
+    //adds a new value before  a targeted value
+    preEnd(newVal,targetVal){
         if(this.isEmpty()){
             console.log("list is empty")
             return false
@@ -240,8 +236,7 @@ class SinglyLinkedList{
             this.insertAtFront(newVal)
             console.log("new node added before node with the value of",targetVal )
             return true
-        }
-        while(current.next !== null){
+        }while(current.next !== null){
             previous = current
             current = current.next
             if(targetVal == current.data){
@@ -252,11 +247,83 @@ class SinglyLinkedList{
             }
         }
     }
+
+
+    //joins a pass in list to the end of  the curent list 
+    concat(newList){
+        if(this.isEmpty()){
+            this.head =  newList.head
+            console.log(this.head)
+            this.size += newList.size
+            return this
+        }else{
+            let current = this.head;
+            while(current.next !== null){
+                    current = current.next
+                }
+            current.next = newList.head
+            this.size += newList.size
+            console.log(this.size)
+            return this
+        }
+    }
+
+
+    //gets the minimum value of the list and adds it to the front on ly worker on listData that are numbers
+    minToFront(){
+        if(this.isEmpty()){
+            return this
+        }else{
+            let current = this.head
+            let previous;
+            let min = current;
+            while(current.next !== null){
+                previous = current
+                // console.log(previous)
+                if(min.data  <  current.data){
+                    min = current.next
+                    break
+                }
+                current = current.next
+            }
+            let toAdd = min.data
+            this.removeVal(min.data)
+            this.insertAtFront(toAdd)
+        }
+    }
+
+
+    //splits the current list into 2 list at the given value
+    splitOnVal(val){
+        if(this.isEmpty()){
+            return this
+        }else{
+            let current = this.head
+            let previous;
+            let count = 0
+            while(current.next !== null){
+                previous = current
+                count++
+                console.log(count)
+                if(val == previous.data){
+                    const splittedList = new SinglyLinkedList().head = previous.next
+                    // previous.nex = null
+                    console.log("this is the splitted list ",splittedList)
+                    // this.size = count
+                    // console.log(splittedList.size)
+                    return splittedList
+                }
+                current = current.next
+            }
+        }
+    }
+
+
     //!this is not part of the algo is a practice to se if you undertand 
     //come up whit a way that you can take a node name(head name) fined it and insert a new node in its .next and also in the new node that you just inserted you need to move the data from the previouse . next to the nex node  so basicaly if that node that you look for has data in its next property you need to get that data store itthan replace it with the new node to be inserted then in that node that you just inserted you need to put the data that you store in to its . next 
         insertAtNodeName(){}
 
-//some comments
+//adds all the data in the list to an array
     toArr(){
         const arr = []
         let runner = this.head
@@ -267,6 +334,8 @@ class SinglyLinkedList{
         }
         return arr
     }
+
+    //prints the info of the list 
     printListData(){
         //this creates a variable assign to be the the first head(node)
         let current = this.head;
@@ -274,7 +343,7 @@ class SinglyLinkedList{
         while(current){
             //we log the values of the head(nodes) 
             // console.log("full node:",current)
-            console.log("node number:",current.data)
+            console.log("node number:",current)
             console.log("______________")
             //we assign current to be the next node(head) 
             current =current.next;
@@ -288,24 +357,42 @@ class SinglyLinkedList{
 
 
 const myList = new SinglyLinkedList(); // create a new list
+const secondList = new SinglyLinkedList();
 
  // insert 1 at the back of the list
 console.log(myList.isEmpty())
-myList.insertAtFront(1)
-myList.insertAtBack(2); // insert 1 at the back of the list
 myList.insertAtFront(0)
-myList.insertAtBack(3)
-myList.insertAtFront(-1)
-myList.removeHead()
-myList.average()
-// myList.removeBack()
-// myList.contains(3)
-myList.containsRecursive(1)
-myList.secondToLast()
-// myList.removeVal(1)
-myList.preEnd(1.5,0)
+myList.insertAtBack(1); // insert 1 at the back of the list
+// myList.insertAtFront()
+secondList.insertAtBack(-1)
+secondList.insertAtBack(2)
+// myList.insertAtFront(-1)
+// myList.removeHead()
+// myList.average()
+myList.removeVal(-10)
+// // myList.removeBack()
+// // myList.contains(3)
+// myList.containsRecursive(1)
+// myList.secondToLast()
+// // myList.removeVal(1)
+// myList.preEnd(1.5,0)
 
+
+myList.concat(secondList)
+// myList.printListData()
+// console.log(myList.toArr()); // output: [1, 2, 3, 5]
+
+// secondList.printListData()
+// console.log(secondList.toArr()); // output: [1, 2, 3, 5]
+
+
+// myList.minToFront()
+
+myList.splitOnVal(0)
 
 myList.printListData()
 console.log(myList.toArr()); // output: [1, 2, 3, 5]
+
+// secondList.printListData()
+// console.log(secondList.toArr()); // output: [1, 2, 3, 5]
 
